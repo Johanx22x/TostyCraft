64eab47f3ce55d9dc7c5706118ee464a85af1283
Johanx22x <johanrodsa2210@gmail.com>
2023-07-12 04:48:27 -0600

    TostyCraft v1.1: Fix error while smelting water and fit food values to a better experience.

--- src/main/java/com/johan/tostycraft/TostyCraft.java
+++ src/main/java/com/johan/tostycraft/TostyCraft.java
@@ -1,16 +1,17 @@
 package com.johan.tostycraft;
 
 import com.johan.tostycraft.item.TostyItems;
-import com.mojang.logging.LogUtils;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TostyCraft.MOD_ID)
@@ -19,9 +20,6 @@ public class TostyCraft
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tostycraft";
 
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-
     // Create DeferredRegister instances for items
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
@@ -29,22 +27,42 @@ public class TostyCraft
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the common setup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
         // Register the DeferredRegister objects
         ITEMS.register(modEventBus);
 
         // Register TostycraftItems
         TostyItems.TOSTY_ITEMS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
+    // Add a listener for the event that fires when a player smelts
+    // a water bucket, to give them an empty bucket back
+    @SubscribeEvent
+    public void onItemSmelted(PlayerEvent.ItemSmeltedEvent event)
     {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
+        // Check if the item smelted was a salt item
+        if (event.getSmelting().getItem() == TostyItems.SALT_ITEM.get())
+        {
+            // Create a new item stack with a bucket
+            ItemStack itemStack = new ItemStack(Items.BUCKET); // Create a new item stack with a bucket
+
+            // Check if the player has room in their inventory
+            if (hasSpaceForEmptyBucket(event.getPlayer())) {
+                // Add the bucket to the player's inventory
+                event.getPlayer().getInventory().add(itemStack);
+            } else {
+                // Drop the bucket in the world
+                event.getPlayer().drop(itemStack, false);
+            }
+        }
+    }
+
+    public boolean hasSpaceForEmptyBucket(Player player) {
+        for (ItemStack itemStack : player.getInventory().items) {
+            if (itemStack.getItem() == Items.BUCKET || itemStack.isEmpty()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
